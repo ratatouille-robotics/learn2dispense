37c256b6a650170aa8689ea5bc792c3c8a0538ed
Sai Shruthi Balaji <=>
2022-11-05 22:54:39 -0400

    Pick and place routine to automate data collection

--- src/learn2dispense/env.py
+++ src/learn2dispense/env.py
@@ -29,6 +29,11 @@ class Environment:
         self.dispenser = Dispenser(self.robot_mg)
         self.container_wt = self.refill_container()
 
+        self.pose1 = [-0.14, -0.485, 0.42, 0.7071068, 0.0, 0.0, 0.7071068]
+        self.pose2 = [-0.29, -0.47, 0.42, 0.7071068, 0.0, 0.0, 0.7071068]
+
+        self.pose_on_scale = [0, 0, 0, 0, 0, 0]
+
         assert self.robot_mg.go_to_joint_state(
             self.HOME, cartesian_path=True, velocity_scaling=0.15
         )
@@ -37,6 +42,68 @@ class Environment:
         config_dir = pathlib.Path(__file__).parent.parent.parent
         with open(config_dir / "config/lentil_params.yaml") as f:
             self.ingredient_params = yaml.safe_load(f)
+
+    def pick_container(self, pick_pose):
+        # Open gripper
+        self.robot_mg.open_gripper(wait=False)
+
+        # Intermediate pose?
+
+        # Go to designated pose
+        if not self.robot_mg.go_to_pose_goal(
+            pick_pose,
+            acc_scaling=0.1,
+            velocity_scaling=0.9,
+        ):
+            print("Error moving to pose goal")
+            return
+
+        # Close gripper
+        self.robot_mg.close_gripper(wait=False)
+
+    def place_container(self, place_pose):
+        # Intermediate pose?
+
+        # Go to designated pose
+        if not self.robot_mg.go_to_pose_goal(
+            place_pose,
+            acc_scaling=0.1,
+            velocity_scaling=0.9,
+        ):
+            print("Error moving to pose goal")
+            return
+
+        # Open gripper and place container
+        self.robot_mg.open_gripper(wait=False)
+
+    def pick_and_place_container(self, pick_pose, place_pose):
+        # Open gripper
+        self.robot_mg.open_gripper(wait=False)
+
+        # Go to intermediate pose?
+
+        # Go to pose where container is
+        if not self.robot_mg.go_to_pose_goal(
+            pick_pose,
+            acc_scaling=0.1,
+            velocity_scaling=0.9,
+        ):
+            print("Error moving to pose goal")
+            return
+
+        # Grip the container
+        self.robot_mg.close_gripper(wait=True)
+
+        # Go to intermediate pose?
+
+        # Place container on weighing scale
+        if not self.robot_mg.go_to_pose_goal(
+            place_pose,
+            acc_scaling=0.1,
+            velocity_scaling=0.9,
+        ):
+            print("Error moving to pose goal")
+            return
 
     def refill_container(self) -> float:
         user_input = float(input("Please refill container.\nEnter the weight of the container: "))
@@ -55,9 +122,23 @@ class Environment:
 
         while(current_steps < total_steps):
 
-            if(self.container_wt < self.REFILL_THRESHOLD):
-                self.container_wt = self.refill_container()
+            if current_steps == 0:
+                # To start with, pick and place empty container on weighing scale
+                self.pick_and_place_container(self.pose1, self.pose_on_scale)
+                # Pick full container for dispensing
+                self.pick_container(self.pose2)
 
+            if(self.container_wt < self.REFILL_THRESHOLD):
+                # Place empty container on shelf
+                self.place_container(self.pose1)
+
+                # TODO: Read container weight?
+                # Replace full container onto shelf
+                self.pick_and_place_container(self.pose_on_scale, self.pose2)
+                # Place empty container on weighing scale
+                self.pick_and_place_container(self.pose1, self.pose_on_scale)
+
+            # Proceed with dispensing
             target_wt = self.sample_weight()
             rospy.loginfo(f"Requested wt: {target_wt:0.4f}")
             success, dispensed_wt, rollout_data = self.dispenser.dispense_ingredient(
@@ -72,19 +153,24 @@ class Environment:
                         data[k].append(v)
                     else:
                         data[k] = [v]
+<<<<<<< HEAD
                 
                 current_steps += len(rollout_data["obs"])
+=======
+
+                current_steps += len(rollout_data["state"])
+>>>>>>> Pick and place routine to automate data collection
 
         for k, v in data.items():
             data[k] = np.concatenate(v, axis=0)
-        
+
         return data
 
     @property
     def observation_space(self) -> gym.spaces.Space:
         observation_space = gym.spaces.Box(
             low=-np.inf,
-            high=np.inf, 
+            high=np.inf,
             shape=(13,),
             dtype=np.float32
         )
